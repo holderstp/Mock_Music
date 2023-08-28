9b0ce66b13cbb62788249e6349df64440db5e129
Minh Hieu <tmhieu.rose@gmail.com>
2023-08-28 19:31:43 +0700

    hieuupdate

--- src/Pages/SearchPage.tsx
+++ src/Pages/SearchPage.tsx
@@ -3,6 +3,6 @@ import { useParams } from "react-router-dom";
 const SearchPage = () => {
   const params = useParams();
   console.log(params);
-  return <div>search page ne</div>;
+  return <div>search page ne abc</div>;
 };
 export default SearchPage;
